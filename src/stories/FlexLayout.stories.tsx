508e2cbc3fde13aefe5ab981689819921ac95ef3
bennyweise <ben.weise@gmail.com>
2023-01-25 09:02:15 +1100

    Simple model that works in storybook

---
+++ src/stories/FlexLayout.stories.tsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+// import FlexLayout from '../ts/components/FlexLayout';
+import { FlexLayout } from '../ts/index';
+// import FlexLayout from "flexlayout-react";
+
+import 'flexlayout-react/style/light.css'
+
+var json = {
+    global: {tabEnableClose:false, tabEnableFloat: true},
+    borders:[
+        {
+          "type": "border",
+          "location":"bottom",
+          "size": 100,
+          "children": [
+              {
+              "type": "tab",
+              "name": "four",
+              "component": "text"
+              }
+            ]
+        },
+    		{
+          "type": "border",
+          "location":"left",
+          "size": 100,
+          "children": []
+         }
+    ],
+    layout: {
+        "type": "row",
+        "weight": 100,
+        "children": [
+            {
+                "type": "tabset",
+                "weight": 50,
+                "selected": 0,
+                "children": [
+                    {
+                        "type": "tab",
+                        "name": "One",
+                        "component": "text",
+                        "enableFloat": true,
+                        "id": "tab-1",
+                    }
+                ]
+            },
+            {
+                "type": "tabset",
+                "weight": 50,
+                "selected": 0,
+                "children": [
+                    {
+                        "type": "tab",
+                        "name": "Two",
+                        "component": "text",
+                        "id": "tab-2",
+                    },
+                    {
+                        "type": "tab",
+                        "name": "Three",
+                        "component": "text",
+                        "id": "tab-3",
+                    }
+                ]
+            }
+        ]
+    }
+};
+
+export default {
+    title: 'Layout/FlexLayout',
+    component: FlexLayout,
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    argTypes: {
+      backgroundColor: { control: 'color' },
+    },
+  } as ComponentMeta<typeof FlexLayout>;
+  
+  // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+  const Template: ComponentStory<typeof FlexLayout> = (args) => <FlexLayout {...args} />;
+  
+  export const Primary = Template.bind({});
+  // More on args: https://storybook.js.org/docs/react/writing-stories/args
+  Primary.args = {
+    primary: true,
+    label: 'FlexLayout',
+    config: json,
+    nodes: {
+        "tab-1": <div>{"something here"}</div>,
+        "tab-2": <div>{"something here in tab 2"}</div>,
+        "tab-3": <div>{"something here in tab 3"}</div>,
+    }
+  };
+  
--- src/ts/components/FlexLayout.tsx
+++ src/ts/components/FlexLayout.tsx
@@ -1,19 +1,36 @@
 import React from 'react';
 import {DashComponentProps} from '../props';
+import * as CaplinFlexLayout from "flexlayout-react";
+import { IJsonModel } from 'flexlayout-react';
 
 type Props = {
-    // Insert props
+    /**
+     * Layout config
+     */
+    config: CaplinFlexLayout.IJsonModel,
+
+    /**
+     * Layout nodes
+     */
+    nodes: {string: React.ReactChild }
 } & DashComponentProps;
 
 /**
  * Component description
  */
 const FlexLayout = (props: Props) => {
-    const { id } = props;
+    const { id, config, nodes } = props;
+    const modelConfig = CaplinFlexLayout.Model.fromJson(config);
+
+    const factory = (node: CaplinFlexLayout.TabNode) => {
+        console.log(node);
+        console.log(node.getChildren());
+        console.log(node.getComponent());
+        // return <div>{"This is a node"}</div>
+        return <div>{nodes[node.getId()]}</div>
+    }
     return (
-        <div id={id}>
-            {"This is a FlexLayout component"}
-        </div>
+        <CaplinFlexLayout.Layout model={modelConfig} factory={factory}/>
     )
 }
 
